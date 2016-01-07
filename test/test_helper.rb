83f73217c20c312fab280327a99e3e0dc6c87db9
Edwin Velasquez <edwin.velaquez@gmail.com>
2016-01-07 01:00:22 -0500

    Add enviroment file.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'cloudconvert-ruby'
 require 'minitest/autorun'
 require 'webmock/minitest'
-require 'lib/enviroment.rb'
+require 'lib/environment.rb'
 #WebMock.allow_net_connect!
 #WebMock.disable_net_connect!(allow_localhost: true)
